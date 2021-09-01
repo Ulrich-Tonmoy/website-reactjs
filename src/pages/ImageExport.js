692fd9772e5a9e440c79e30000f4b009f84cd4ff
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2021-09-01 19:58:31 +0600

    remove file

--- src/pages/ImageExport.js
+++
@@ -1,9 +0,0 @@
-import React from "../images/logo.svg";
-import Color from "../images/color.png";
-import Home from "../images/home.png";
-import Stripe from "../images/stripe.png";
-
-export const ReactLogo = React;
-export const ColorImg = Color;
-export const HomeLogo = Home;
-export const StripeImg = Stripe;
