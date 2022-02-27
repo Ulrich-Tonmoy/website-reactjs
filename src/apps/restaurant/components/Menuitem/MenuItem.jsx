3a2b9344d0cae7e85d1d1969efc9cd3f0b80cdf2
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-02-28 02:06:28 +0600

    specialmenu finished

--- src/apps/restaurant/components/Menuitem/MenuItem.jsx
+++ src/apps/restaurant/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import "./MenuItem.css";
 
-import './MenuItem.css';
+export default function MenuItem({ title, price, tags }) {
+    return (
+        <div className="restaurant__menuitem">
+            <div className="restaurant__menuitem-head">
+                <div className="restaurant__menuitem-name">
+                    <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+                        {title}
+                    </p>
+                </div>
+                <div className="restaurant__menuitem-dash" />
+                <div className="restaurant__menuitem-price">
+                    <p className="p__cormorant">{price}</p>
+                </div>
+            </div>
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
-);
-
-export default MenuItem;
+            <div className="restaurant__menuitem-sub">
+                <p className="p__opensans" style={{ color: "#AAAAAA" }}>
+                    {tags}
+                </p>
+            </div>
+        </div>
+    );
+}
