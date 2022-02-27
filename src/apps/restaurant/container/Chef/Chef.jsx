313bfb96d6ff4c76afb8ca23470c116036c63880
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-02-28 02:09:12 +0600

    chef finished

--- src/apps/restaurant/container/Chef/Chef.jsx
+++ src/apps/restaurant/container/Chef/Chef.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import "./Chef.css";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-import './Chef.css';
+export default function Chef() {
+    return (
+        <div className="restaurant__bg restaurant__wrapper section__padding">
+            <div className="restaurant__wrapper_img restaurant__wrapper_img-reverse">
+                <img src={images.chef} alt="chef_image" />
+            </div>
+            <div className="restaurant__wrapper_info">
+                <SubHeading title="Chef's word" />
+                <h1 className="headtext__cormorant">What we believe in</h1>
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+                <div className="restaurant__chef-content">
+                    <div className="restaurant__chef-content_quote">
+                        <img src={images.quote} alt="quote_image" />
+                        <p className="p__opensans">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
+                        </p>
+                    </div>
+                    <p className="p__opensans">
+                        {" "}
+                        auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla
+                        scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu.
+                        Congue iaculis integer curabitur semper sit nunc.{" "}
+                    </p>
+                </div>
 
-export default Chef;
+                <div className="restaurant__chef-sign">
+                    <p>Kevin Luo</p>
+                    <p className="p__opensans">Chef & Founder</p>
+                    <img src={images.sign} alt="sign_image" />
+                </div>
+            </div>
+        </div>
+    );
+}
