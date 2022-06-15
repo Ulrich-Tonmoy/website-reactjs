229a744098a3e206d7afeee0f5238a4c0d1e843f
Tonmoy <ulrichtonmoy1@gmail.com>
2022-06-16 04:23:39 +0600

    to top button

--- fitness-zone/src/App.jsx
+++ fitness-zone/src/App.jsx
@@ -5,8 +5,25 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ExerciseDetails from "./pages/ExerciseDetails";
 import Footer from "./components/Footer";
+import { useState } from "react";
+import { ArrowUpward } from "@mui/icons-material";
 
 function App() {
+    const [displayButton, setDisplayButton] = useState("");
+
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            setDisplayButton("block");
+        } else {
+            setDisplayButton("none");
+        }
+    };
+
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
     return (
         <Router>
             <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
@@ -17,6 +34,9 @@ function App() {
                 </Routes>
             </Box>
             <Footer />
+            <button onClick={topFunction} id="top" style={{ display: displayButton }}>
+                <ArrowUpward />
+            </button>
         </Router>
     );
 }
