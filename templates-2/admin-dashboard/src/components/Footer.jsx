5dced336f15fe6b35f56f965ac977382ec824edf
Tonmoy <ulrichtonmoy1@gmail.com>
2022-06-20 04:47:07 +0600

    ✅ admin-dashboard

---
+++ templates-2/admin-dashboard/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => (
+    <div className="mt-24">
+        <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+            © 2022 All rights reserved by
+            <a
+                href="https://github.com/Ulrich-Tonmoy"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: "none", marginLeft: 4 }}
+            >
+                Ulrich Tonmoy
+            </a>
+        </p>
+    </div>
+);
+
+export default Footer;
